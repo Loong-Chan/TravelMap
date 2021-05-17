7c0fd4750522d10935ee5c8389fc9b1a77a7238e
loongchan <243403254@qq.com>
2021-05-17 20:05:40 +0800

    1

--- source/Meta.py
+++ source/Meta.py
@@ -1,9 +1,25 @@
 import json
+import time
 
 
-MetaFilePath = ""
+MetaFilePath = "./META.json"
+Meta = None
 
-def getMeta():
-    with open(MetaFilePath, "r") as f:
-        data = json.load(f)
-        print(data)
+
+def readMeta():
+    with open(MetaFilePath, "r", encoding='utf-8') as f:
+        global Meta
+        Meta = json.loads(f.read())
+
+
+def writeMeta():
+    with open(MetaFilePath, "w", encoding="utf-8") as f:
+        global Meta
+        cTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        Meta["Last update"] = cTime
+        json.dump(Meta, f, indent=4, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    readMeta()
+    writeMeta()
