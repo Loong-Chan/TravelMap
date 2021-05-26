486066db00f9202c49917773e9f692fb572a1437
loongchan <243403254@qq.com>
2021-05-26 21:38:53 +0800

    0.0.2

--- source/Console.py
+++ source/Console.py
@@ -3,16 +3,18 @@
 
 
 from math import ceil
+from abc import ABC, abstractmethod
 
 
-class Console:
+class Console(ABC):
     # 目前只支持纯英文
 
     def __init__(self, width: int = 70) -> None:
         self._width = width
         self._boxBuffer = ""
-        self._pages = []
+        self._pages = [self.pageExit]
         self._select = 0
+        self._pageNameToIndex = {"Exit": 0}
 
     def newBox(self) -> None:
         self._boxBuffer = ""
@@ -36,57 +38,84 @@ class Console:
         self._boxBuffer = "*" * self._width + "\n" + self._boxBuffer
         self._boxBuffer = self._boxBuffer + "*" + " " * (self._width - 2) + "*\n"
         self._boxBuffer = self._boxBuffer + "*" * self._width + "\n"
-        print(self._boxBuffer)
+        print(self._boxBuffer, end="")
 
-    def pageManager(self) -> None:
+    def startUp(self) -> None:
+        self.define()
         while(True):
             self._pages[self._select](self._select)
 
-    def newPage(self, pageFunc) -> int:
-        if not callable(pageFunc):
-            raise Exception()
-        self._pages.append(pageFunc)
-        return len(self._pages) - 1
+    def newPage(self, pageFunc, pageName: str) -> int:
+        if not callable(pageFunc) or pageFunc.__code__.co_argcount == 0:
+            raise Exception("page is not in the right format.")
+        if pageName in self._pageNameToIndex:
+            raise Exception("page name has been used.")
 
-    def setJump(self, pageIndex: int) -> None:
-        self._select = pageIndex
+        self._pageNameToIndex[pageName] = len(self._pages)
+        self._pages.append(pageFunc)
+
+        return self._pageNameToIndex[pageName]
+
+    def setJump(self, pageInfo) -> None:
+        if isinstance(pageInfo, int):
+            if pageInfo >= len(self._pages):
+                raise Exception("Index out of range.")
+            self._select = pageInfo
+            return
+        if isinstance(pageInfo, str):
+            if pageInfo not in self._pageNameToIndex:
+                raise Exception("no such page name")
+            self._select = self._pageNameToIndex[pageInfo]
+            return
+        raise Exception("page info type error.")
 
     def inputOper(self) -> list:
         '''从键盘读取一次操作及其参数'''
         paras = [p.strip() for p in input().split(" -")]
         return paras[0], paras[1:]
 
-    def dispatcher(self, oper: str, args: list, vaildOper: dict) -> bool:
+    def dispatcher(self, oper: str, argList: list, vaildOper: dict) -> bool:
         """通过分派器把对应的操作和参数分派给指定函数执行"""
         if oper not in vaildOper:
-            return self.operDefault(args)
+            return self.operDefault(argList)
         else:
-            return vaildOper[oper](args)
+            return vaildOper[oper](argList)
 
+    # 在初始化时需要有一个默认页，只有退出的功能，每次退出程序都不是直接退出，而是跳转到默认页，由默认页执行退出任务
+    def pageExit(self, pageIndex: int) -> None:
+        exit(0)
+
+    def operExit(self, argList: list):
+        self.setJump(0)
+
+    def operDefault(self, argList) -> None:
+        return None
+
+    @abstractmethod
+    def define(self) -> None:
+        pass
+
+
+'''
     def pageExample(self, pageIndex: int) -> None:
         vaildOper = {"a": self.examFunc1, "b": self.examFunc2}
 
-        '''
-        xxx
-        '''
+
+        # xxx
+
 
         while pageIndex == self._select:
-            '''
-            xxx
-            '''
+
+            # xxx
+
             oper, args = self.inputOper()
             self.dispatcher(oper, args, self._vaildOperUser)
-
-    # 在初始化时需要有一个默认页，只有退出的功能，每次退出程序都不是直接退出，而是跳转到默认页，由默认页执行退出任务
-    def pageExit(self) -> None:
-        pass
-
 
     def examFunc1(self, args: list) -> None:
         print(args[0])
         print(args[1])
         self.setJump(1)
-
+'''
 
 if __name__ == "__main__":
     c = Console()
--- source/metroconsole.py
+++ source/metroconsole.py
@@ -1,129 +1,131 @@
-# 我在想Console要设计成互斥的，就是一个文件只能开一个console，然后有了console就不用每次都要运行命令
-# 而且可以分管理员模式和普通用户模式，普通用户只能查询，管理员可以添加修改，可以同时多个用户操作，但是只能有一个管理员操作
-
-
-from time import sleep
 from Metro import Metro
 from MetaController import MetaController
 from ProjectTools import getProjectPath
-from math import ceil
+from Console import Console
 import os
 
 
-class MetroConsole:
+class MetroConsole(Console):
 
-    consoleWidth = 70
     password = "123456"
 
     def __init__(self) -> None:
-        self._administrator = False
+
+        super(MetroConsole, self).__init__()
+
         self._file = None
         self._meta = MetaController()
-        self._isAdmin = False
         self._metro = None
-        self._vaildOperMain = {"admin": self.operAdmin,
-                               "user": self.operUser, "exit": self.operExit}
-        self._vaildOperUser = {"logout": self.operLogout, "list": self.operList,
-                               "load": self.operLoad, "query": self.operQuery}
-        self._vaildOperAdmin = {"logout": None, "list": None, "new": None, "add": None, "save": None,
-                                "load": None, "create": None, "query": None}
-        # 运行控制台
-        self.console()
 
-    def console(self) -> None:
-        # 首界面
-        while(True):
-            os.system("cls")
-            self.printMeta()
-            print(" Please select the operation Mode(user/admin):", end="")
-            oper, args = self.inputOper()
-            if self.dispatcher(oper, args, self._vaildOperMain):
-                break
-        # 主要操作界面
-        os.system("cls")
-        self.printHint()
-        print(" enter you operation:", end="")
-        while(True):
-            oper, args = self.inputOper()
-            os.system("cls")
-            self.printHint()
-            self.dispatcher(oper, args, self._vaildOperUser)
-            print(" enter you operation:", end="")
-
-    def printStr(self, string: str, shift: int = None) -> None:
-        """格式化输出一行，其中shift指从左往右的位移，默认居中"""
-        # 计算左空格数
-        if shift is None:  # 居中的情况
-            blankLenL = ceil((MetroConsole.consoleWidth - 2 - len(string)) / 2)
-        else:  # 左对齐的情况
-            blankLenL = shift
-        # 输出字符串
-        blankLenR = MetroConsole.consoleWidth - 2 - blankLenL - len(string)
-        if(blankLenL <= 0 or blankLenR <= 0):
-            print("error! console width too small")
-            return
-        print("*" + " " * blankLenL + string + " " * blankLenR + "*")
+    def define(self) -> None:
+        self.newPage(self.pageMain, "Main")
+        self.newPage(self.pageAdmin, "Admin")
+        self.newPage(self.pageUser, "User")
+        self.setJump("Main")
 
     def printMeta(self) -> None:
-        """输出主界面中的软件的元信息"""
-        print("*" * MetroConsole.consoleWidth)
-        print("*" + " " * (MetroConsole.consoleWidth - 2) + "*")
-        self.printStr(self._meta.getNameEN())
-        self.printStr("developer: " + self._meta.getDeveloper())
-        self.printStr("code version: " + self._meta.getCodeVersion())
-        self.printStr("data version: " + self._meta.getDataVersion())
-        print("*" + " " * (MetroConsole.consoleWidth - 2) + "*")
-        print("*" * MetroConsole.consoleWidth)
+        self.newBox()
+        self.addContentToBox(self._meta.getNameEN())
+        self.addContentToBox("developer: " + self._meta.getDeveloper())
+        self.addContentToBox("code version: " + self._meta.getCodeVersion())
+        self.addContentToBox("data version: " + self._meta.getDataVersion())
+        self.drawBox()
 
-    def printHint(self) -> None:
-        """输出使用界面中的提示操作信息"""
-        print("*" * MetroConsole.consoleWidth)
-        print("*" + " " * (MetroConsole.consoleWidth - 2) + "*")
-        self.printStr("logout: lot out and return to the main interface", 5)
-        self.printStr(
-            "list  : list the infomation of the designated station or line", 5)
-        self.printStr("new   : build a new station or line", 5)
-        self.printStr("add   : add a station to a line:", 5)
-        self.printStr("save  : save the latest data to a file", 5)
-        self.printStr("load  : load an existing data file", 5)
-        self.printStr("create: create a new data file", 5)
-        self.printStr("query : query the line between any station", 5)
-        print("*" + " " * (MetroConsole.consoleWidth - 2) + "*")
-        print("*" * MetroConsole.consoleWidth)
+    def printAdminHint(self) -> None:
+        self.newBox()
+        self.addContentToBox("logout: lot out and return to the main interface", 5)
+        self.addContentToBox("list  : list the infomation of the designated station or line", 5)
+        self.addContentToBox("new   : build a new station or line", 5)
+        self.addContentToBox("add   : add a station to a line:", 5)
+        self.addContentToBox("save  : save the latest data to a file", 5)
+        self.addContentToBox("load  : load an existing data file", 5)
+        self.addContentToBox("create: create a new data file", 5)
+        self.addContentToBox("query : query the line between any station", 5)
+        self.drawBox()
 
-    def inputOper(self) -> list:
-        '''从键盘读取一次操作及其参数'''
-        paras = [p.strip() for p in input().split(" -")]
-        return paras[0], paras[1:]
+    def printUserHint(self) -> None:
+        self.newBox()
+        self.addContentToBox("logout: lot out and return to the main interface", 5)
+        self.addContentToBox("list  : list the infomation of the designated station or line", 5)
+        self.addContentToBox("load  : load an existing data file", 5)
+        self.addContentToBox("query : query the line between any station", 5)
+        self.drawBox()
 
-    def dispatcher(self, oper: str, args: list, vaildOper: dict) -> bool:
-        """通过分派器把对应的操作和参数分派给指定函数执行"""
-        if oper not in vaildOper:
-            return self.operDefault(args)
-        else:
-            return vaildOper[oper](args)
+    def pageMain(self, pageIndex: int) -> None:
 
-    def operAdmin(self, args: list) -> bool:
-        self._isAdmin = True
-        return True
+        vaildOper = {"user": self.operTurnToUser, "admin": self.operTurnToAdmin,
+                     "exit": self.operExit}
 
-    def operUser(self, args: list) -> bool:
-        self._isAdmin = False
-        return True
+        while pageIndex == self._select:
+            os.system("cls")
+            self.printMeta()
+            print("\n Please select the operation Mode(user/admin):", end="")
+            oper, argList = self.inputOper()
+            self.dispatcher(oper, argList, vaildOper)
 
-    def operExit(self, args: list) -> None:
-        exit(0)
+    def pageUser(self, pageIndex: int) -> None:
 
-    def operDefault(self, args: list) -> bool:
-        return False
+        vaildOper = {"logout": self.operLogout, "list": self.operList,
+                     "new": self.operNew, "add": self.operAdd,
+                     "save": self.operSave, "load": self.operLoad,
+                     "create": self.operCreate, "query": self.operQuery}
 
-    def operLoad(self, args: list) -> bool:
+        os.system("cls")
+        self.printUserHint()
+        print("\n Enter you operation:", end="")
+        while pageIndex == self._select:
+            oper, argList = self.inputOper()
+            os.system("cls")
+            self.printUserHint()
+            self.dispatcher(oper, argList, vaildOper)
+            print("\n Enter you operation:", end="")
+
+    def pageAdmin(self, pageIndex: int) -> None:
+
+        vaildOper = {"logout": self.operLogout, "list": self.operList,
+                     "load": self.operLoad, "query": self.operQuery}
+
+        os.system("cls")
+        self.printAdminHint()
+        print("\n Enter you operation:", end="")
+        while pageIndex == self._select:
+            oper, argList = self.inputOper()
+            os.system("cls")
+            self.printAdminHint()
+            self.dispatcher(oper, argList, vaildOper)
+            print("\n Enter you operation:", end="")
+
+    def operTurnToUser(self, argList: list):
+        self.setJump("User")
+
+    def operTurnToAdmin(self, argList: list):
+        print(" Please enter your password:", end="")
+        if input() == MetroConsole.password:
+            self.setJump("Admin")
+
+    def operLogout(self, argList: list):
+        self.setJump("Main")
+
+    def operList(self, argList: list):
+
+        stationNames = self._metro.getStationsName()
+        lineNames = self._metro.getLinesName()
+        stationOrder = self._metro.getStationOrder()
+
+        self.newBox()
+        for i in range(len(lineNames)):
+            self.addContentToBox("line: {}".format(lineNames[i]), 5)
+            for j in stationOrder[i]:
+                self.addContentToBox("{}-->".format(stationNames[j]), 10)
+        self.drawBox()
+
+    def operLoad(self, argList: list):
 
         if self._file is not None:
             print(" you have already opened a file.")
             return False
 
-        filePath = getProjectPath() + "/citys/{}.txt".format(args[0])
+        filePath = getProjectPath() + "/citys/{}.txt".format(argList[0])
         if not os.path.exists(filePath):
             print(" file doesn't exist.")
             return False
@@ -131,41 +133,30 @@ class MetroConsole:
         print(" load data from file: {}...".format(filePath), end="")
         self._metro = Metro(filePath)
         print(" done!")
-        return True
 
-    # 目前还没想好怎么具体的实现登出时页面的切换
-    def operLogout(self, args: list) -> bool:
-        exit(0)
-
-    def operList(self, args: list) -> bool:
+    def operQuery(self, argList: list):
 
         stationNames = self._metro.getStationsName()
-        lineNames = self._metro.getLinesName()
-        stationOrder = self._metro.getStationOrder()
-        print("*" * MetroConsole.consoleWidth)
-        print("*" + " " * (MetroConsole.consoleWidth - 2) + "*")
-        for i in range(len(lineNames)):
-            self.printStr("line: {}".format(lineNames[i]), 5)
-            for j in stationOrder[i]:
-                self.printStr("{}-->".format(stationNames[j]), 10)
-        print("*" + " " * (MetroConsole.consoleWidth - 2) + "*")
-        print("*" * MetroConsole.consoleWidth)
-        return True
+        path, weight = self._metro.findPath(argList[0], argList[1])
 
-    def operQuery(self, args: list) -> bool:
-        stationNames = self._metro.getStationsName()
-        path, weight = self._metro.findPath(args[0], args[1])
-        print("*" * MetroConsole.consoleWidth)
-        print("*" + " " * (MetroConsole.consoleWidth - 2) + "*")
-        self.printStr("You need to take {} stops in the subway".format(weight), 5)
+        self.newBox()
+        self.addContentToBox("You need to take {} stops in the subway".format(weight), 5)
         for s in path:
-            self.printStr("{}-->".format(stationNames[s]), 10)
-        print("*" + " " * (MetroConsole.consoleWidth - 2) + "*")
-        print("*" * MetroConsole.consoleWidth)
-        return True
+            self.addContentToBox("{}-->".format(stationNames[s]), 10)
+        self.drawBox()
+
+    def operNew(self, argList: list):
+        pass
+
+    def operAdd(self, argList: list):
+        pass
+
+    def operSave(self, argList: list):
+        pass
+
+    def operCreate(self, argList: list):
+        pass
 
 
 if __name__ == "__main__":
-    MetroConsole()
-
-# 考虑使用装饰器
+    MetroConsole().startUp()
